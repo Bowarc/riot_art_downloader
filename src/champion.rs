d6e7fb880da7d2ef82d35fc7ab90dcd5ef7bfeae
Bowarc <yuyeaaaahhhh@laposte.net>
2021-11-20 21:30:50 +0100

    Done for League Of Legends.

--- src/champion.rs
+++ src/champion.rs
@@ -2,15 +2,16 @@ use serde::{Deserialize, Serialize};
 
 use std::collections::HashMap;
 
-#[derive(Serialize, Deserialize, Default, Clone)]
+#[derive(Serialize, Deserialize, Default, Clone, Debug)]
 pub struct ChampionList {
-    pub r#type: String,
+    #[serde(rename = "type")]
+    pub _type: String,
     pub format: String,
     pub version: String,
     pub data: HashMap<String, Champion>,
 }
 
-#[derive(Serialize, Deserialize, Default, Clone)]
+#[derive(Serialize, Deserialize, Default, Clone, Debug)]
 pub struct Champion {
     pub version: String,
     pub id: String,
@@ -25,7 +26,7 @@ pub struct Champion {
     pub stats: Stats,
 }
 
-#[derive(Serialize, Deserialize, Default, Clone)]
+#[derive(Serialize, Deserialize, Default, Clone, Debug)]
 pub struct Info {
     pub attack: f32,
     pub defense: f32,
@@ -33,7 +34,7 @@ pub struct Info {
     pub difficulty: f32,
 }
 
-#[derive(Serialize, Deserialize, Default, Clone)]
+#[derive(Serialize, Deserialize, Default, Clone, Debug)]
 pub struct Image {
     pub full: String,
     pub sprite: String,
@@ -44,7 +45,7 @@ pub struct Image {
     pub h: f32,
 }
 
-#[derive(Serialize, Deserialize, Default, Clone)]
+#[derive(Serialize, Deserialize, Default, Clone, Debug)]
 pub struct Stats {
     hp: f32,
     hpperlevel: f32,
@@ -66,4 +67,87 @@ pub struct Stats {
     attackdamageperlevel: f32,
     attackspeedperlevel: f32,
     attackspeed: f32,
+}
+
+#[derive(Serialize, Deserialize, Default, Clone, Debug)]
+pub struct DetailedChampionList {
+    #[serde(rename = "type")]
+    pub _type: String,
+    pub format: String,
+    pub version: String,
+    pub data: HashMap<String, DetailedChampion>,
+}
+
+// #[derive(Serialize, Deserialize, Default, Clone, Debug)]
+// pub struct DetailedChampion {
+//     #[serde(rename = "@champ_name")]
+//     pub champ_name: DetailedChampion1,
+// }
+
+#[derive(Serialize, Deserialize, Default, Clone, Debug)]
+pub struct DetailedChampion {
+    pub id: String,
+    pub key: String,
+    pub name: String,
+    pub title: String,
+    pub image: Image,
+    pub skins: Vec<Skin>,
+    pub lore: String,
+    pub blurb: String,
+    pub allytips: Vec<String>,
+    pub enemytips: Vec<String>,
+    pub tags: Vec<String>,
+    pub info: Info,
+    pub stats: Stats,
+    pub spells: Vec<Spell>,
+    pub passive: Passive,
+    pub recommended: Vec<String>, // ???
+}
+
+#[derive(Serialize, Deserialize, Default, Clone, Debug)]
+pub struct Skin {
+    pub id: String,
+    pub num: f32,
+    pub name: String,
+    pub chromas: bool,
+}
+
+#[derive(Serialize, Deserialize, Default, Clone, Debug)]
+pub struct Spell {
+    pub id: String,
+    pub name: String,
+    pub description: String,
+    pub tooltip: String,
+    pub leveltip: Option<LevelTip>,
+    pub maxrank: f32,
+    pub cooldown: Vec<f32>,
+    #[serde(rename = "cooldownBurn")]
+    pub cooldown_burn: String,
+    pub cost: Vec<f32>,
+    #[serde(rename = "costBurn")]
+    pub cost_burn: String,
+    pub datavalues: HashMap<i32, String>, // ???
+    pub effect: Vec<Option<Vec<f32>>>,
+    #[serde(rename = "effectBurn")]
+    pub effect_burn: Vec<Option<String>>,
+    pub vars: Vec<String>,
+    #[serde(rename = "costType")]
+    pub cost_type: String,
+    pub maxammo: String,
+    pub range: Vec<f32>,
+    pub image: Image,
+    pub resource: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Default, Clone, Debug)]
+pub struct Passive {
+    pub name: String,
+    pub description: String,
+    pub image: Image,
+}
+
+#[derive(Serialize, Deserialize, Default, Clone, Debug)]
+pub struct LevelTip {
+    pub label: Vec<String>,
+    pub effect: Vec<String>,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-// struct riot_bg_downloader {}
-// use serde_json::Value; //Result
-
 use sublime_fuzzy::{FuzzySearch, Scoring};
+use colored::Colorize;
 use std::io;
+use image;
 
 mod champion;
 
 const DDRAGON_VERSION_URL: &str = "https://ddragon.leagueoflegends.com/api/versions.json";
 const CHAMPION_DATA_URL: &str = "https://ddragon.leagueoflegends.com/cdn/%VERSION%/data/%LANGUAGE%/champion/%CHAMPION%.json";
 const LANGUAGE_URL: &str = "https://ddragon.leagueoflegends.com/cdn/languages.json";
-const CHAMPION_LIST_URL:&str = "https://ddragon.leagueoflegends.com/cdn/%VERSION%/data/%LANGUAGE%/champion.json";
+const CHAMPION_LIST_URL: &str = "https://ddragon.leagueoflegends.com/cdn/%VERSION%/data/%LANGUAGE%/champion.json";
+const CHAMPION_SKIN_URL: &str = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/%CHAMPION%_%SKIN_NUMBER%.jpg";
 
 fn get_closest_match(input: String, data: Vec<String>) -> String{
     fn new_best_match(score: isize, name: &str) -> (isize, String) {
@@ -43,7 +43,7 @@ fn get_closest_match(input: String, data: Vec<String>) -> String{
     best_match.1
 }
 
-async fn request_by_url(url: &str) -> String{
+async fn request_by_url(url: &str) -> reqwest::Response{
     let r: reqwest::Response = reqwest::get(url).await.expect(&format!("Couldn't get a response for the given url: \n {}.", url));
 
     if r.status() != 200{
@@ -51,24 +51,19 @@ async fn request_by_url(url: &str) -> String{
         println!("Problem,\nStatus: {}\nURL: {}", r.status(), url);
     }
 
-    let response_text: String = r.text().await.expect(&format!("Couldn't get the text from the response."));
+    r
 
-    response_text
 }
 
 async fn get_ddragon_version() -> String{
-    let version_data: String = request_by_url(DDRAGON_VERSION_URL).await;
+    let version_data: String = request_by_url(DDRAGON_VERSION_URL).await.text().await.unwrap();
     let version_list: Vec<String> = serde_json::from_str(&version_data).expect(&format!("Couldn't transfrom version data to vec of string."));
-
-    // self.ddragon_latest_version = version_list[0].clone();
-
-    // println!("version: {}", self.ddragon_latest_version);
 
     return version_list[0].clone()
 }
 
 async fn get_language_list() -> Vec<String> {
-    let languages_data: String = request_by_url(LANGUAGE_URL).await;
+    let languages_data: String = request_by_url(LANGUAGE_URL).await.text().await.unwrap();
     let language_list: Vec<String> = serde_json::from_str(&languages_data).expect(&format!("Couldn't create a json object form the response's text."));
 
     language_list
@@ -76,40 +71,39 @@ async fn get_language_list() -> Vec<String> {
 
 async fn ask_language(language_list: Vec<String>) -> String {
     if language_list.is_empty(){
-            panic!("Yikes, the language_list is empty");
+        panic!("Yikes, the language_list is empty");
+    }
+    let mut selected_language = String::new();
+
+    while selected_language == String::new(){
+        for lang in language_list.chunks(5){
+            let s = format!("{:02?}", lang).replace('"', "").replace("[", "").replace("]", "").replace(" ", "  ");
+
+            println!("{}",s);
         }
-        let mut selected_language = String::new();
+        println!("Please choose a language from the list above ^:");
 
-        while selected_language == String::new(){
-            for lang in language_list.chunks(5){
-                let s = format!("{:02?}", lang).replace('"', "").replace("[", "").replace("]", "").replace(" ", "  ");
+        let user_input = ask_input();
 
-                println!("{}",s);
-            }
-            println!("Please choose a language from the list above ^:");
-
-            let user_input = ask_input();
-
-            let closest_match = get_closest_match(user_input, language_list.clone());
-            if closest_match == String::new(){
-                println!("Failes to recognise the selected_language.");
-            }else{
-                selected_language = closest_match;
-            }
+        let closest_match = get_closest_match(user_input, language_list.clone());
+        if closest_match == String::new(){
+            println!("Failes to recognise the selected_language.");
+        }else{
+            selected_language = closest_match;
         }
-        selected_language
-
+    }
+    selected_language
 }
 
 async fn get_champion_list(selected_language: String, ddragon_version: String) ->  champion::ChampionList{
     let url = CHAMPION_LIST_URL.replace("%LANGUAGE%", &selected_language).replace("%VERSION%", &ddragon_version);
 
-    let champion_data: String = request_by_url(&url).await;
+    let champion_data: String = request_by_url(&url).await.text().await.unwrap();
 
     let champion_list: champion::ChampionList = serde_json::from_str(&champion_data).unwrap();
     
     champion_list
-    // self.champion_list = champion_list;
+
 }
 
 fn ask_input() -> String{
@@ -133,21 +127,35 @@ async fn main() {
 
     let mut sorted_champ_list: Vec<String> = champion_list.data.keys().cloned().collect();
     sorted_champ_list.sort();
-    for champ in sorted_champ_list.iter(){
+    for champ_name in sorted_champ_list.iter(){
+        println!("{}", champ_name.underline());
         let url = CHAMPION_DATA_URL
-            .replace("%CHAMPION%", &champ)
+            .replace("%CHAMPION%", &champ_name)
             .replace("%LANGUAGE%", &selected_language)
-            .replace("%VERSION%", &last_ddragon_version);
-        println!("url: {}", url);
+            .replace("%VERSION%", &last_ddragon_version);        
+        let detailled_champion_data_string = request_by_url(&url).await;
+
+        let detailled_champion
+        : champion::DetailedChampionList = serde_json::from_str(&detailled_champion_data_string.text().await.unwrap()).unwrap();
+
+        let champion = detailled_champion.data.get(champ_name).unwrap().clone();
+
+        for (_index, skin) in champion.skins.iter().enumerate(){
+            println!("{}\n", skin.name);
+
+            let skin_url = CHAMPION_SKIN_URL
+                .replace("%CHAMPION%", champ_name)
+                .replace("%SKIN_NUMBER%", &skin.num.to_string());
+
+            let img_string = request_by_url(&skin_url).await;
+
+            let image_buffer = img_string.bytes().await.unwrap(); //image::load_from_memory(&img_string.as_bytes()).unwrap();
+
+            let img  = image::load_from_memory(&image_buffer).unwrap();
+
+            let path = format!("img/{}_{}.png", champ_name, skin.name);
+
+            img.save(path).unwrap();
+        }
     }
 }
-
-// for (champ_name, _champ_data) in champion_list.data.iter(){
-//     c +=1;
-//     let url = CHAMPION_DATA_URL
-//         .replace("%CHAMPION%", &champ_name)
-//         .replace("%LANGUAGE%", &selected_language)
-//         .replace("%VERSION%", &last_ddragon_version);
-//     println!("url: {}", url);
-//     // let skin_list = request_by_url(&url).await;
-// }
